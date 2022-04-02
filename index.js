adcd4faf17ffe9d48f77c89ec89734225240652a
Allyson Vitug <allysonvitugg@gmail.com>
2022-04-01 17:38:38 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const jest = require('jest');
+
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
+
